5cf12980428ce7f4b41039e2ba1d2875274b14c4
camclark <cameron.clark1@my.jcu.edu.au>
2016-08-12 10:48:50 +1000

    Setting up

--- Prac03/lect_prac02.py
+++ Prac03/lect_prac02.py
@@ -1,10 +1,9 @@
 def square(number):
-	return number ** 2
-
+    return number ** 2
 
 
 def main():
-    number =
+    number = int(input("please enter a number"))
+    print(number, "^2=", square(number))
+
 main()
-
-
