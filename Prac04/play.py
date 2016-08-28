c735e414d1167bd6448a794bed51c39cf98575a8
camclark <cameron.clark1@my.jcu.edu.au>
2016-08-28 19:27:45 +1000

    comments added to code space formatting for function_print_specific changed to match assignment requirement

--- Prac04/play.py
+++ Prac04/play.py
@@ -1,10 +1,9 @@
 def main():
-    a = [2, 3, 4]
-    b = ["z","x","c"]
-    c = []
-    c.append(a)
-    c.append(b)
-    print(c[0][0])
+    item_name = input("Item name: ")
+    while item_name == "" or " " in item_name:
+        print("Input can not be blank")
+        item_name = input("Item name: ")
 
-
+    item_name = item_name[0].upper() + item_name[1:].lower()
+    print(item_name)
 main()
