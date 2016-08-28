eef5fd1db7c2f209de170127f777682c96605227
camclark <cameron.clark1@my.jcu.edu.au>
2016-08-28 15:38:30 +1000

    unstable: mark items does not mark correct item fixed: mark_items will now only take selections from 0 to number of selections -1

--- Prac04/play.py
+++ Prac04/play.py
@@ -1,16 +1,8 @@
 def main():
-    valid_input = False
-    while not valid_input:
-        try:
-            age = int(input("Enter age: "))
-            if age < 3 or age > 6:
-                print("wot")
-            else:
-                print("good")
-
-        except:
-            print("Invalid not an interger")
-
+    try:
+        item_price = float(input("Price: "))
+    except (TypeError, ValueError):
+        print("Please enter a number")
 
 
 main()
