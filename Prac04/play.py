a28c4c195776cf73099f7dee431e8ae18da914ed
camclark <cameron.clark1@my.jcu.edu.au>
2016-08-28 18:01:01 +1000

    mark_as_compelted function now marks selected item as completed

--- Prac04/play.py
+++ Prac04/play.py
@@ -1,8 +1,10 @@
 def main():
-    try:
-        item_price = float(input("Price: "))
-    except (TypeError, ValueError):
-        print("Please enter a number")
+    a = [2, 3, 4]
+    b = ["z","x","c"]
+    c = []
+    c.append(a)
+    c.append(b)
+    print(c[0][0])
 
 
 main()
