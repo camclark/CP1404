e3df70d31a6c4b3d3cc81081853389d9ca683672
camclark <cameron.clark1@my.jcu.edu.au>
2016-09-01 10:33:35 +1000

    fixed logical error collection_of_words

--- Prac05/collection_of_words.py
+++ Prac05/collection_of_words.py
@@ -7,12 +7,17 @@ words_to_count = input("Text: ")
 words_to_count = words_to_count.split(" ")
 
 counted_words = {}
+longest_key = ""
 
 for word in words_to_count:
     if word in counted_words:
         counted_words[word] += 1
+
     else:
         counted_words[word] = 1
 
-for word,word_count in counted_words.items():
-    print("{:<10} : {:>5}".format(word,word_count))
+        if len(word) > len(longest_key):
+            longest_key = word
+
+for word, word_count in counted_words.items():
+    print("{:<{}} : {}".format(word, len(longest_key), word_count))
--- Prac05/hex_colours.py
+++ Prac05/hex_colours.py
@@ -6,7 +6,7 @@ Colour Hex values in a dictionary
 COLOUR_NAMES = {"AliceBlue": "f0f8ff", "Black": "000000", "CadetBlue": "5f9ea0", "Chocolate": "d2691e",
                 "CornflowerBlue": "6495ed", "DarkGreen": "006400", "DarkSalmon": "e9967a"}
 
-colour = input("Enter short colour: ")
+colour = input("Enter colour: ")
 while colour != "":
     if colour in COLOUR_NAMES:
         print(colour, "is", COLOUR_NAMES[colour])
